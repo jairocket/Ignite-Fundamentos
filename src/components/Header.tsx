cb0b677d3fe7252830d9a3994e52f45c4ecd4634
Jailson Anjos <jailsonanjos@hotmail.com>
2022-10-15 10:49:19 -0300

    feat(header): Show header logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,12 @@
 //@ts-ignore
 import styles from "./Header.module.css";
+//@ts-ignore
+import igniteLogo from "../assets/ignite-logo.svg";
 
 export function Header() {
   return (
     <header className={styles.header}>
+      <img src={igniteLogo} />
       <strong>Ignite Feed</strong>
     </header>
   );
