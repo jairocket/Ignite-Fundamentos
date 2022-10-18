b7cbfff83a6b4a040446ff7b73ac5a1c0dbd161c
Jailson Anjos <jailsonanjos@hotmail.com>
2022-10-17 21:36:19 -0300

    feat(sidebar) Add sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post";
+import { Sidebar } from "./components/Sidebar";
+
 import "./global.css";
+//@ts-ignore
+import styles from "./App.module.css";
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post author="Diego Fernandes" content="conteudo do post" />
-      <Post author="Jai Anjos" content="conteudo do post" />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post author="Diego Fernandes" content="conteudo do post" />
+          <Post author="Jai Anjos" content="conteudo do post" />
+        </main>
+      </div>
+
       <Post />
     </div>
   );
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,28 @@
+//@ts-ignore
+import styles from "./Sidebar.module.css";
+import { PencilLine } from "phosphor-react";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1584947114710-0e62c574950b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+      />
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://avatars.githubusercontent.com/u/71157451?v=4"
+        />
+        <strong>Jailson Anjos</strong>
+        <span>Web Developer</span>
+        <footer>
+          <a href="#">
+            <PencilLine size={20} />
+            Editar seu perfil
+          </a>
+        </footer>
+      </div>
+    </aside>
+  );
+}
